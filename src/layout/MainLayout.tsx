d2e092732546f0ae77e53a9a9d233520421ba363
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-01-31 19:01:05 +0330

    add sidebar and topnav component to the mainLayout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,21 @@
 import classes from "./MainLayout.module.scss";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/sidebar/Sidebar";
+import TopNav from "../components/topnav/TopNav";
+
 
 const MainLayout = () => {
 
     return (
         <div className={classes.container}>
-            لایه اصلی
+        <Sidebar />
+        <div className={classes.main}>
+          <div>
+            <TopNav />
+            <Outlet />
+          </div>
         </div>
+      </div>
     );
 };
 
