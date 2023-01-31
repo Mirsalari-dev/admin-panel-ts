9b6979ff5a4744fcd195b3cce269695ef81ab53c
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-01-31 22:51:31 +0330

    add translate profile name

--- src/components/topnav/rightBox/profile/Profile.tsx
+++ src/components/topnav/rightBox/profile/Profile.tsx
@@ -1,7 +1,12 @@
 import classes from "./Profile.module.scss";
 import avt from "../../../../assets/images/avt.jpg"
+import { useTranslation } from "react-i18next";
+
 
 function Profile() {
+
+  const { t } = useTranslation()
+
 
   return (
     <div className={classes.profile}>
@@ -9,7 +14,7 @@ function Profile() {
         <img src={avt} alt="avatar" />
       </div>
       <div className={classes.profile__info}>
-        <p className={classes.profile__userName}>Amirhossein Mirsalari</p>
+        <p className={classes.profile__userName}>{t("AmirhosseinMirsalari")}</p>
         {/* <span className={classes.profile__role}>{t("admin")}</span> */}
       </div>
     </div>
