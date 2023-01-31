e35e3d60787f010e3e03054462d9ee92f75c1b04
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-01-31 18:46:08 +0330

    create layout folder

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import classes from "./MainLayout.module.scss";
+
+const MainLayout = () => {
+
+    return (
+        <div className={classes.container}>
+            لایه اصلی
+        </div>
+    );
+};
+
+export default MainLayout;
