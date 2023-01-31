8b76793e9d3defef9ab4103fb737d7044873d885
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-01 00:23:11 +0330

    styling card component

--- src/components/UI/card/Card.tsx
+++ src/components/UI/card/Card.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import classes from "./Card.module.scss";
+
 
 interface Props {
   children: React.ReactNode;
 }
 const Card: React.FC<Props> = (props) => {
-  return <div>{props.children}</div>;
+  return <div className={classes.card}>{props.children}</div>;
 };
 
 export default Card;
