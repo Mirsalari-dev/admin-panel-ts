dc85e85b84c548cde1d5277055c277b41bf9f2cf
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-10 22:32:52 +0330

    complete style alarm box

--- src/components/topnav/rightBox/alarmBox/AlarmBox.tsx
+++ src/components/topnav/rightBox/alarmBox/AlarmBox.tsx
@@ -23,11 +23,23 @@ const AlarmBox = () => {
                 <span className={classes.alarm__span__text}>{t("3")}</span>
             </div>
             <div className={`${classes.alarm__menu} ${showAlarmBox ? classes.alarm__menu__show : ""}`}>
-                <ul>
-                    <li>213</li>
-                    <li>123123</li>
-                    <li>12312321</li>
+                <ul className={classes.alarm__menu__ul}>
+                    <li>یک پیغام جدید دارید
+                        <Icon style={{position:"absolute",right:"10px",top:"12px"}} icon="ph:bell-ringing-duotone" width="20" />
+                    </li>
+                    <li>یک پیغام جدید دارید
+                    <Icon style={{position:"absolute",right:"10px",top:"58px"}} icon="ph:bell-ringing-duotone" width="20" />
+                    </li>
+                    <li>یک پیغام جدید دارید
+                    <Icon style={{position:"absolute",right:"10px",top:"104px"}} icon="ph:bell-ringing-duotone" width="20" />
+                    </li>
                 </ul>
+                <div className={classes.alarm__menu__allpm}>
+                    مشاهده همه پیام ها
+                    <div>
+                        <Icon icon="material-symbols:arrow-back-ios-rounded" />
+                    </div>
+                </div>
             </div>
 
 
