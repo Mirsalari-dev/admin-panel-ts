926cc19d83031f0c72defbe36f5ef167c6ade4cb
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-10 22:35:25 +0330

    add checkIfClickedOutside into alarmBox

--- src/components/topnav/rightBox/alarmBox/AlarmBox.tsx
+++ src/components/topnav/rightBox/alarmBox/AlarmBox.tsx
@@ -1,17 +1,31 @@
 import { Icon } from '@iconify/react';
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import classes from "./AlarmBox.module.scss";
 import { useTranslation } from 'react-i18next';
+import { useOnClickOutside } from "usehooks-ts";
+
 
 
 const AlarmBox = () => {
     const { t } = useTranslation()
+    const alarmBoxRef = useRef<HTMLDivElement>(null);
     const [showAlarmBox, setShowAlarmBox] = useState(false);
 
 
     const showBoxHandler = function toDo() {
         setShowAlarmBox((prev) => !prev);
     };
+
+    const checkIfClickedOutside = useCallback(() => {
+        // If the menu is open and the clicked target is not within the menu,
+        // then close the menu
+        if (showAlarmBox && alarmBoxRef.current) {
+            setShowAlarmBox(false);
+        }
+      }, [showAlarmBox]);
+    
+      //custom hook - when click outside of langbox, it will close.
+      useOnClickOutside(alarmBoxRef, checkIfClickedOutside);
 
 
     return (
@@ -22,7 +36,7 @@ const AlarmBox = () => {
             <div className={classes.alarm__span}>
                 <span className={classes.alarm__span__text}>{t("3")}</span>
             </div>
-            <div className={`${classes.alarm__menu} ${showAlarmBox ? classes.alarm__menu__show : ""}`}>
+            <div ref={alarmBoxRef} className={`${classes.alarm__menu} ${showAlarmBox ? classes.alarm__menu__show : ""}`}>
                 <ul className={classes.alarm__menu__ul}>
                     <li>یک پیغام جدید دارید
                         <Icon style={{position:"absolute",right:"10px",top:"12px"}} icon="ph:bell-ringing-duotone" width="20" />
