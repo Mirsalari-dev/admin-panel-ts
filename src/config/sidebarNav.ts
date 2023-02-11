8b18ce05996aef026c712f3eb36bf9aeb1ec8ed3
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-11 15:51:02 +0330

    add children to the shop section in sidebarNav

--- src/config/sidebarNav.ts
+++ src/config/sidebarNav.ts
@@ -10,6 +10,22 @@ const sidebarNav = [
     section: "shop",
     icon: "icon-park-outline:ad-product",
     text: "Shop",
+    children:[{
+      link: "/",
+      section: "dashboard",
+      icon: "lucide:layout-dashboard",
+      text: "All Products",
+    },{
+      link: "/",
+      section: "dashboard",
+      icon: "lucide:layout-dashboard",
+      text: "Add Product",
+    },{
+      link: "/",
+      section: "dashboard",
+      icon: "lucide:layout-dashboard",
+      text: "Create discount code",
+    },]
   },
   {
     link: "/customers",
