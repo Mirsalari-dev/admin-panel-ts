eaecb4ab0f1300eab7a3e53ae596fe5c0d6148bf
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-13 01:01:03 +0330

    edit customTables for adding customers

--- src/components/tables/customTable/CustomTable.tsx
+++ src/components/tables/customTable/CustomTable.tsx
@@ -4,10 +4,16 @@ import Card from "../../UI/card/Card";
 import { useTranslation } from "react-i18next";
 import classes from "./CustomTable.module.scss";
 import Badge from "../../UI/badge/Badge";
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
 
 const CustomTable: React.FC<Props> = (props) => {
 
-  
+  const [showModal, setShowModal] = useState(false);
+  function showModalHandler() {
+    setShowModal((prev) => !prev);
+  }
+
   function tableBody(item: complex, index: number) {
     /* type guard (in typescript) */
     if ("username" in item) {
@@ -32,10 +38,84 @@ const CustomTable: React.FC<Props> = (props) => {
           </td>
         </tr>
       );
-    }
+    }else if ("email" in item) {
+      //for implementing customers table
+      return (
+        <tr key={index}>
+          <td>{item.ID}</td>
+          <td className={classes.userName}>
+            <img
+              className={classes.avatar}
+              src={item.avatar}
+              alt="user avatar"
+            />
+            {item.userName}
+          </td>
+          <td className="ltr">{item.email}</td>
+          <td className="ltr">{item.phoneNumber}</td>
+          <td>{item.totalOrders}</td>
+          <td>{item.totalSpend}</td>
+          <td>{item.location}</td>
+          <td className={classes.actions}>
+            <Icon icon="charm:menu-kebab" />
+            <div className={classes.actions__box}>
+              <div
+                className={classes.actions__delete}
+                onClick={showModalHandler}
+              >
+                <Icon icon="fluent:delete-24-regular" width="24" />
+              </div>
+              <div className={classes.actions__edit}>
+                <Link to={`/customers/${item.ID}`}>
+                  <Icon icon="fluent:edit-16-regular" width="24" />
+                </Link>
+              </div>
+            </div>
+          </td>
+        </tr>
+      );
+    } 
+  }
+  const initDataShow = () => {
+    return props.limit && props.bodyData
+      ? props.bodyData.slice(0, Number(props.limit))
+      : props.bodyData;
+  };
+
+  const [dataShow, setDataShow] = useState(initDataShow);
+  // const [selectedCategory, setSelectedCategory] = useState(
+  //   props.selectedCategory
+  // );
+
+  // if (props.selectedCategory) {
+  //   if (selectedCategory !== props.selectedCategory)
+  //     setDataShow(props.bodyData);
+  // }
+  // setSelectedCategory(props.selectedCategory);
+
+  let pages = 1;
+
+  let range: number[] = [];
+
+  if (props.limit !== undefined) {
+    let page = Math.floor(props.bodyData.length / Number(props.limit));
+    pages = props.bodyData.length % Number(props.limit) === 0 ? page : page + 1;
+    range = [...Array(pages).keys()];
   }
 
+  const [currPage, setCurrPage] = useState(0);
+
+  const selectPage = (page: number) => {
+    const start = Number(props.limit) * page;
+    const end = start + Number(props.limit);
+
+    setDataShow(props.bodyData?.slice(start, end));
+
+    setCurrPage(page);
+  };
+
   const { t } = useTranslation();
+
 
   return (
     <>
