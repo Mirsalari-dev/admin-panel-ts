7899244a3c32d8344a75139aa0c1246e20a2f50f
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-14 17:33:41 +0330

    complete customers edit page

--- src/pages/CustomerEdit.tsx
+++ src/pages/CustomerEdit.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
-
+import EditCustomer from "../components/edit/editCustomer/EditCustomer";
+import { IcustomersTable } from "../interfaces/Itable";
+import  data  from "../mock/tables"
 
 function CustomerEdit() {
   const { t } = useTranslation();
   const params = useParams();
   let { customerId } = params;
 
+  /* fallback in case of time limit to test firebase database will over */
+  let customerInfo: IcustomersTable = data.customers.filter(
+    (item) => item.ID.toString() === customerId
+  )[0];
+
+  let customerEdit =<EditCustomer customer={customerInfo} />
+
 
   return (
     <section>
       <h2 className="title">{t("editCustomer")}</h2>
+      {customerEdit}
     </section>
   );
 }
