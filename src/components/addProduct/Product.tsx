962c68eab30eb39feeddc3a4322ae33b4076d5fc
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-22 22:38:41 +0330

    add title section and text editor in add product page

--- src/components/addProduct/Product.tsx
+++ src/components/addProduct/Product.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Editors from '../editor/Editors';
+import Input from '../UI/input/Input';
 import classes from "./Product.module.scss";
 
 
 const Product = () => {
     return (
         <div>
-            add product page
+            <div>
+                <label htmlFor="product-title" className={classes.title}>عنوان محصول</label>
+                <input id="product-title" type='text' className={classes.input_title} />
+            </div>
+            <Editors />
         </div>
     );
 };
