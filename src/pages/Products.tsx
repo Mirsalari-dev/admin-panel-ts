0831a30966d75a00ce82a7b58b1a565539895ad6
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-28 01:51:44 +0330

    add search bar in products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import CustomTable from "../components/tables/customTable/CustomTable";
 import useTitle from "../helper/useTitle";
 import data from "../mock/tables";
-
+import SearchContext from "../context/searchTerm";
+import searchTerm from "../helper/searchTerm";
 
 
 function Products() {
   const { t } = useTranslation();
   useTitle(t("products"))
 
+  const { search } = useContext(SearchContext)
+  const [filtered, setFiltered] = useState<any>([]);
+
+  useEffect(() => {
+    setFiltered(searchTerm(data.products, search))
+  }, [search])
+
+
+
   let productsTable = <CustomTable
     headData={data.productsHeader}
-    bodyData={data.products}
+    bodyData={filtered}
     limit={10}
+    key={filtered}
   />
 
   return (
