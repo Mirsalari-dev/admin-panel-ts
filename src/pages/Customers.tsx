062e7b1d1fe1f4102432a141970c11ed7b048eba
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-28 01:39:32 +0330

    add search bar in customers page

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import CustomTable from "../components/tables/customTable/CustomTable";
 import useTitle from "../helper/useTitle";
 import data from "../mock/tables";
+import SearchContext from "../context/searchTerm";
+import searchTerm from "../helper/searchTerm";
 
 
 function Customers() {
   const { t } = useTranslation();
   useTitle(t("customers"))
 
-  let customerTable = <CustomTable limit={10} headData={data.customersHeader} bodyData={data.customers} />
+  const { search } = useContext(SearchContext)
+  const [filtered, setFiltered] = useState<any>([]);
+
+  useEffect(() => {
+    setFiltered(searchTerm(data.customers, search))
+  }, [search])
+
+  let customerTable = <CustomTable limit={10} headData={data.customersHeader} bodyData={filtered} key={filtered} />
 
   return (
     <section>
