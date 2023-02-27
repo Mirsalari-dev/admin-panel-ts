16f4f710c06e3f73ca6eb6d964fcd7d8361367b4
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-02-28 01:42:33 +0330

    add search bar in transactions page

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import CustomTable from "../components/tables/customTable/CustomTable";
 import useTitle from "../helper/useTitle";
 import data from "../mock/tables";
-
+import SearchContext from "../context/searchTerm";
+import searchTerm from "../helper/searchTerm";
 
 
 function Transactions() {
   const { t } = useTranslation();
   useTitle(t("transactions"))
-  let transactionsTable = <CustomTable
-    headData={data.latestOrders.header}
-    bodyData={data.latestOrders.body}
-    limit={10}
-  />
+  const { search } = useContext(SearchContext)
+  const [filtered, setFiltered] = useState<any>([]);
+
+  useEffect(() => {
+    setFiltered(searchTerm(data.latestOrders.body, search))
+  }, [search])
+
+
+  let transactionsTable = <CustomTable headData={data.latestOrders.header} bodyData={filtered} limit={10} key={filtered} />
 
   return (
     <section>
