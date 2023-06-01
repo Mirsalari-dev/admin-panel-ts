2b387df7dd6a474c34eb999a194a98ee46e3d009
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-01 17:46:26 +0330

    add updateCustomer reducer to handle update informations customers

--- src/redux/customersSlice.ts
+++ src/redux/customersSlice.ts
@@ -1,12 +1,12 @@
-import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import data from "../mock/tables";
-import {IcustomersTable} from "../interfaces/Itable"
+import { IcustomersTable } from "../interfaces/Itable";
 
-type InitialState =  {
-  customers : IcustomersTable[]
-}
+type InitialState = {
+  customers: IcustomersTable[];
+};
 
-const initialState : InitialState = {
+const initialState: InitialState = {
   customers: data.customers,
 };
 
@@ -14,14 +14,34 @@ const customersSlice = createSlice({
   name: "customers",
   initialState,
   reducers: {
-    removeCustomer : (state , action: PayloadAction<any>)=>{
-        state.customers = state.customers.filter(customer => customer.ID != action.payload)
-        
-        
-    }
+    removeCustomer: (state, action: PayloadAction<any>) => {
+      state.customers = state.customers.filter(
+        (customer) => customer.ID != action.payload
+      );
+    },
+    updateCustomer: (state, action: PayloadAction<any>) => {
+      let updated = [...state.customers];
+      const updatedItemIndex = updated.findIndex(
+        (item) => item.ID === action.payload.id
+      );
+      if (updatedItemIndex < 0) {
+        updated = [...state.customers];
+      } else {
+        let updatedItem = { ...updated[updatedItemIndex] };
+        updatedItem.userName = action.payload.userName
+        updatedItem.phoneNumber = action.payload.phoneNumber
+        updatedItem.email = action.payload.email
+        updatedItem.location = action.payload.location
+        updatedItem.avatar = action.payload.avatar
+        updated[updatedItemIndex] = updatedItem;
+      }
+      return {
+        ...state,
+        customers: updated,
+      };
+    },
   },
 });
 
 export default customersSlice.reducer;
-export const { removeCustomer } = customersSlice.actions
-
+export const { removeCustomer, updateCustomer } = customersSlice.actions;
