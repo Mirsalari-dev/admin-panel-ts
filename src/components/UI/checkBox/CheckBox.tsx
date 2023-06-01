65b1acd952ab751ad5801e1614d089cdeb1cc7aa
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-01 22:45:57 +0330

    add onclick and active state to props of chackBox

--- src/components/UI/checkBox/CheckBox.tsx
+++ src/components/UI/checkBox/CheckBox.tsx
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import classes from "./CheckBox.module.scss"
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import classes from "./CheckBox.module.scss";
 
 interface Props {
-  contentActive?: string
-  contentInActive?: string
-
+  contentActive?: string;
+  contentInActive?: string;
+  onClick?: any;
+  active?: string;
 }
 
-
 const CheckBox: React.FC<Props> = (props) => {
-  const [active, setActive] = useState(false)
+  // const [active, setActive] = useState(false);
 
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   return (
-    <div style={{display:"flex",}}>
-      <div className={classes.themeBox} onClick={() => setActive(active => !active)}>
+    <div style={{ display: "flex" }}>
+      <div
+        className={classes.themeBox}
+        onClick={props.onClick}
+      >
         <div
-          className={`${classes.toggle} ${active ? classes.darkMode : ""
-            }`}
+          className={`${classes.toggle} ${props.active=="Confirmed" ? classes.darkMode : ""}`}
         ></div>
       </div>
       {/* <p>{`${active ? t(`${props.contentActive}`) : t(`${props.contentActive}`)}</p> */}
-      <p>{`${active ? t(`${props.contentActive}`) :t(`${props.contentInActive}`) }`}</p>
+      <p>{`${
+        props.active=="Confirmed" ? t(`${props.contentActive}`) : t(`${props.contentInActive}`)
+      }`}</p>
     </div>
   );
 };
